85b9d7143ea3d18c471c111ebbd6e4f5f9d8856f
Sai Charan Kavuri <saicharankavuri@Sais-Air.lan>
2023-09-17 20:17:35 -0400

    Added D3JS chart

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', express.static('public'))
+app.use('/', express.static('public'))
 
 // app.get('/hello', (req,res) => {
 //     res.send('Hello World!!');
